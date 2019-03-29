3bd68722304823ee0a3ce31dba8698341e4fc8da
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-03-29 16:52:20 +0900

    Add files via upload

---
+++ _02motor_order.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+delay(5000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(6,10); delay(1000);
+analogWrite(10,10); delay(1000);
+analogWrite(9,10); delay(1000);
+analogWrite(5,10); delay(1000);
+
+analogWrite(6,0); delay(1000);
+analogWrite(10,0); delay(1000);
+analogWrite(9,0); delay(1000);
+analogWrite(5,0); delay(1000);
+delay(2000);
+}
---
+++ _03throttle.ino
@@ -0,0 +1,40 @@
+#include <HM10BLE.h>
+#define THROTTLE_IMI 10
+
+HM10BLE Ble;
+
+void setup() {
+  // put your setup code here, to run once:
+Ble.begin(115200);
+init_motor_speed(THROTTLE_IMI);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int roll, pitch, yaw, throttle;
+Ble.receive_user_rpyt(roll, pitch, yaw, throttle);
+
+update_motor_speed(throttle);
+
+}
+
+int motorA_pin = 6;
+int motorB_pin = 10;
+int motorC_pin = 9;
+int motorD_pin = 5;
+
+void init_motor_speed(int speed){
+  analogWrite(motorA_pin, speed);
+  analogWrite(motorB_pin, speed);
+  analogWrite(motorC_pin, speed);
+  analogWrite(motorD_pin, speed);
+}
+
+void update_motor_speed(int speed) {
+  analogWrite(motorA_pin, speed);
+  analogWrite(motorB_pin, 0);
+  analogWrite(motorC_pin, speed);
+  analogWrite(motorD_pin, 0);
+}
+
