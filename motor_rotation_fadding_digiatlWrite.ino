5ea19d9bb4ede7a95d199e0e39101bd00fca48ac
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-04-04 11:24:46 +0900

    Add files via upload

---
+++ motor_rotation_fadding_digiatlWrite.ino
@@ -0,0 +1,28 @@
+const int fan_pin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(fan_pin, OUTPUT);
+}
+
+unsigned long current_millis = 0;
+unsigned long prev_millis = 0;
+unsigned long interval_milis = 1000;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int t_high=0; t_high<=10; t_high++) {
+    while(1) {
+      digitalWrite(fan_pin, HIGH);
+      delay(t_high);
+      digitalWrite(fan_pin, LOW);
+      delay(10-t_high);
+
+      current_millis = millis();
+      if(current_millis - prev_millis >= interval_milis) { 
+        prev_millis = current_millis;
+        break;
+      }
+    }
+  }
+}
---
+++ read_rpy.ino
@@ -0,0 +1,36 @@
+#include <MPU6050Sensor.h>
+
+MPU6050Sensor Sensor;
+
+void setup() {
+  // put your setup code here, to run once:
+  Sensor.begin(0x68);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float roll_angle, pitch_angle, yaw_angle;
+  Sensor.read_rpy_angular_velocity(roll_angle, pitch_angle, yaw_angle);
+  
+  unsigned long current_millis = millis();
+  static unsigned long prev_millis = 0;
+  static unsigned long interval = 100;
+  if(current_millis - prev_millis >= interval) {
+    prev_millis = current_millis;
+    
+    print_rpy_angle(roll_angle, pitch_angle, yaw_angle);
+  }
+}
+
+void print_rpy_angle(float roll_angle, float pitch_angle, float yaw_angle) {
+  Serial.print("#ROY:");
+  Serial.print(roll_angle);
+  Serial.print(",");
+  Serial.print(pitch_angle);
+  Serial.print(",");
+  Serial.print(yaw_angle);
+
+  Serial.println();
+}
+
