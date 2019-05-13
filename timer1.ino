f5f9470d8f9ca1abb3bc4decd56858e1280eb7c3
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-05-14 00:22:07 +0900

    타이머

---
+++ timer1.ino
@@ -0,0 +1,18 @@
+#include <TimerOne.h>
+
+const int fan_pin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  Timer1.initialize();
+  Timer1.pwm(fan_pin, 0);
+
+  Timer1.setPeriod(1000000);
+  Timer1.setPwmDuty(fan_pin, 511);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
